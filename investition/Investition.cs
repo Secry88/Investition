0cd44b6baebc825890098fa56722d0b9f407ee69
Artem <artemka4837@gmail.com>
2025-05-28 23:51:47 +0300

    Реализована инитиализация таблицы и подсчет шагов

---
+++ investition/Investition.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace investition
+{
+    public class Investition
+    {
+        public int[,] initTable(int[,] originTable, int[] step, int[] companies)
+        {
+            int[,] newTable = new int[originTable.GetLength(0) + 1, originTable.GetLength(1) + 1];
+            for (int i = 0; i < originTable.GetLength(0); i++)
+            {
+                for (int j = 0; j < originTable.GetLength(1); j++)
+                {
+                    newTable[0, j + 1] = companies[j];
+                    newTable[i + 1, 0] = step[i];
+                    newTable[i + 1, j + 1] = originTable[i, j];
+                }
+            }
+            return newTable;
+        }
+
+        public void print(int[,] mas)
+        {
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+
+        public void MainMethod(int[,] originTable, int[] step, int[] companies)
+        {
+            int[,] table = initTable(originTable, step, companies);
+            int rows = table.GetLength(0);
+            print(table);
+
+            int summ = 0;
+
+            for (int i = 0; i < step.Length; i++)
+            {
+                int firstStepCount = 2;
+                firstStepCount += i;
+                summ += firstStepCount;
+            }
+
+            List<int[]> stepCalc = new List<int[]>();
+
+            int stepCount = 2;
+            for (int i = 0; i < step.Length; i++)
+            {
+                int a = 0;
+                int b = step[i];
+
+                for (int k = 0; k < stepCount; k++)
+                {
+                    stepCalc.Add(new int[] {step[i], a, b });
+                    a += step[0];
+                    b -= step[0];
+                }
+                stepCount++;
+            }
+            Console.WriteLine("\n");
+            foreach (int[] item in stepCalc)
+            {
+                Console.WriteLine(string.Join("\t", item));
+            }
+        }
+    }
+}
---
+++ investition/Program.cs
@@ -0,0 +1,17 @@
+﻿using investition;
+
+int[,] table =
+{
+    {8,  10,  12,  11 },
+    {16, 20,  21,  23 },
+    {25, 28,  27,  30 },
+    {36, 40,  38,  37, },
+    {44, 48  ,50  ,51 },
+    {62 ,62  ,63  ,63 }
+};
+
+int[] step = { 20, 40, 60, 80, 100, 120 };
+int[] companies = { 1, 2, 3, 4 };
+
+Investition investition = new Investition();
+investition.MainMethod(table, step, companies);
